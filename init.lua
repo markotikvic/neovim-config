6329ae003dd38561b994a8c97e5ec254e94295ca
Marko Tikvic <marko.tikvic@sigmoid.rs>
2023-07-18 15:39:30 +0200

    Replace yanked text

--- init.lua
+++ init.lua
@@ -159,22 +159,23 @@ function config_shortcuts()
 	vim.keymap.set('n', '<leader>fi', vim.lsp.buf.implementation)
 	vim.keymap.set('n', '<leader>fr', vim.lsp.buf.references)
 	vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename)
+	vim.keymap.set('n', '<leader>re', ':%s/<C-r><C-w>//gc<Left><Left><Left>')
+	vim.keymap.set('n', '<leader>Re', ':s/<C-r>"//gc<Left><Left><Left>')
 	vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float)
 	vim.keymap.set('n', '<leader>ds', vim.diagnostic.show)
 	vim.keymap.set('n', '<leader>dh', vim.diagnostic.hide)
-	vim.keymap.set('n', '<leader>re', ':%s/<C-r><C-w>//gc<Left><Left><Left>')
 end
 
 function config_dotnet()
 	local pid = vim.fn.getpid()
 	local home = vim.fn.getenv('HOME')
 
---	require('lspconfig').omnisharp.setup({
---		on_attach = function(_, bufnr)
---			vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
---		end,
---		cmd = { home .. '/.cache/omnisharp-vim/omnisharp-roslyn/run', "--languageserver" , "--hostPID", tostring(pid)};
---	})
+	require('lspconfig').omnisharp.setup({
+		on_attach = function(_, bufnr)
+			vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+		end,
+		cmd = { home .. '/.cache/omnisharp-vim/omnisharp-roslyn/run', "--languageserver" , "--hostPID", tostring(pid)};
+	})
 	vim.g.OmniSharp_highlighting = 0
 end
 
